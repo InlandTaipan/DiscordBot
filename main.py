72fce91d484dee633f09ad34b78e9e1704340a8b
Maximo van der Raadt <mvanderr@edu.uwaterloo.ca>
2020-06-16 21:46:34 -0600

    syncing main

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ load_dotenv()
 
 import os
 import discord
+import asyncio
 
 #My modules
 import coinsAndDice #Functions for flipping a coin and rolling dice
 import help #Display help message
+import connectFour #Discord version of connect four
 
 #Setting up client
 client = discord.Client()
@@ -32,13 +34,16 @@ async def on_message(message):
         return
 
     #flipcoin command
-    if message.content.lower() == "-flipcoin":
+    elif message.content.lower() == "-flipcoin":
         await coinsAndDice.coinflip(message)
     #dice roll command
     elif message.content.lower().startswith("-roll"):
         await coinsAndDice.rollDice(message)
     #help command
-    if message.content.lower() == "-help":
+    elif message.content.lower() == "-help":
         await help.sendHelp(message)
+
+    elif message.content.lower() == "-connectfour":
+        await connectFour.run(message,client)
 
 client.run(TOKEN)
